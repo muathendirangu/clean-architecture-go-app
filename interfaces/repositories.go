bd95de69e431a2fb7c5a044360dec619bab057ea
muathendirangu <muathe.ndirangu@gmail.com>
2020-02-15 10:58:32 +0000

    add database repository struct

--- interfaces/repositories.go
+++ interfaces/repositories.go
@@ -9,3 +9,20 @@ type Row interface {
 	Scan(dest ...interface{})
 	Next() bool
 }
+
+type DBRepository struct {
+	dbHandlers map[string]DBHandler
+	dbHandler  DBHandler
+}
+
+type DBUserRepository DBRepository
+type DBOrderRepository DBRepository
+type DBCustomerRepository DBRepository
+type DBItemRepository DBRepository
+
+func NewDBUserRepository(dbHandlers map[string]DBHandler) *DBUserRepository {
+	dbUserRepository := new(DBUserRepository)
+	dbUserRepository.dbHandlers = dbHandlers
+	dbUserRepository.dbHandler = dbHandlers["DBUserRepository"]
+	return dbUserRepository
+}
